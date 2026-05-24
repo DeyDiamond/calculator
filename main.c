155cc61d4c82526f847f9a7ba80fe9855a7f4f2f
DeyDiamond <back.gr1ndoor@gmail.com>
2026-05-24 21:02:35 +0300

    calculator

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    char op;
+    printf("Enter number: ");
+    scanf("%d", &a);
+    printf("Enter operation: ");
+    scanf(" %c", &op);
+    printf("Enter number: ");
+    scanf("%d", &b);
+
+if (op == '+')
+{
+    printf("Result: %d\n", a + b);
+}
+if (op == '-')
+{
+    printf("Result: %d\n", a - b);
+}
+if (op == '*')
+{
+    printf("Result: %d\n", a * b);
+}
+if (op == '/')
+{
+    if (b == 0)
+    {
+     printf("Error: division by zero\n");
+    }
+    else 
+    {
+    printf("Result: %d\n", a / b);
+    }
+}
+
+return 0;
+}
