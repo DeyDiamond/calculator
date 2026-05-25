38658570b4dec3f146903b150c5ac1d2a1826ab4
DeyDiamond <back.gr1ndoor@gmail.com>
2026-05-25 16:29:00 +0300

    add calculator loop

--- main.c
+++ main.c
@@ -5,40 +5,48 @@ int main(void)
     int a;
     int b;
     char op;
-    printf("Enter number: ");
-    scanf("%d", &a);
-    printf("Enter operation: ");
-    scanf(" %c", &op);
-    printf("Enter number: ");
-    scanf("%d", &b);
+    char again;
 
-if (op == '+')
-{
-    printf("Result: %d\n", a + b);
-}
-else if (op == '-')
-{
-    printf("Result: %d\n", a - b);
-}
-else if (op == '*')
-{
-    printf("Result: %d\n", a * b);
-}
-else if (op == '/')
-{
-    if (b == 0)
-    {
-     printf("Error: division by zero\n");
-    }
-    else 
-    {
-    printf("Result: %d\n", a / b);
-    }
-}
-else
-{
-    printf("Error: Unknown operator\n");    
-}
+    again = 'y';
 
+     while (again == 'y')
+    { 
+        printf("Enter number: ");
+        scanf("%d", &a);
+        printf("Enter operation: ");
+        scanf(" %c", &op);
+        printf("Enter number: ");
+        scanf("%d", &b);
+        
+        if (op == '+')
+        {
+          printf("Result: %d\n", a + b);
+        }
+        else if (op == '-')
+        {
+          printf("Result: %d\n", a - b);
+        }
+        else if (op == '*')
+        {
+          printf("Result: %d\n", a * b);
+        }
+        else if (op == '/')
+        {
+        if (b == 0)
+        {
+          printf("Error: division by zero\n");
+        }
+        else 
+        {
+         printf("Result: %d\n", a / b);
+        }
+        }
+        else
+        {
+         printf("Error: Unknown operator\n");    
+        }
+         printf("Again? y/n:\n");
+         scanf(" %c", &again);
+    }
 return 0;
 }
